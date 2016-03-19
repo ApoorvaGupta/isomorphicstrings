db083754520a9f176bb85565077314d9ffde3621
Apoorva Gupta <gupta08.apoorva@gmail.com>
2016-03-18 20:44:33 -0700

    First commit

--- isomorphicstrings/main.m
+++ isomorphicstrings/main.m
@@ -8,10 +8,50 @@
 
 #import <Foundation/Foundation.h>
 
+
+
+
+BOOL isIsomorphic (NSMutableString* str1, NSMutableString *str2)
+{
+    if(str1.length != str2.length)
+        return NO;
+    else if (str1.length == 0 && str2.length == 0)
+        return YES;
+    else {
+        NSMutableDictionary *isomorphicDictionary = [[NSMutableDictionary alloc]init];
+        for(int i =0; i< str1.length; i++)
+        {
+            NSString *charone = [NSString stringWithFormat:@"%c",[str1 characterAtIndex:i]];
+            NSString *charTwo = [NSString stringWithFormat:@"%c",[str2 characterAtIndex:i]];
+            if([[isomorphicDictionary allKeys] containsObject:charone] && [isomorphicDictionary valueForKey:charone] != charTwo)
+                return NO;
+            else if (![[isomorphicDictionary allKeys] containsObject:charone])
+            {
+                [isomorphicDictionary setObject:charTwo forKey:charone];
+            }
+            
+                
+        }
+       
+    }
+    
+    return YES;
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Hello, Isomorphic Strings!");
+        NSMutableString *stringOne = [[NSMutableString alloc]initWithString:@"turtle"];
+        NSMutableString *stringTwo = [[NSMutableString alloc]initWithString:@"tletur"];
+        
+      BOOL result =  isIsomorphic(stringOne,stringTwo);
+        NSLog(@"%hhd",result);
+        
+        
+        
+        
+        
     }
     return 0;
 }
