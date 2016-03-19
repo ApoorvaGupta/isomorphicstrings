8d3aa0381eacd67f2f38dc42b9ee5f0bc8acd5d0
Apoorva Gupta <apoorvagupta@apoorvas-macbook-pro.local>
2016-03-18 20:08:05 -0700

    Initial Commit

---
+++ isomorphicstrings/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  isomorphicstrings
+//
+//  Created by Apoorva Gupta on 3/18/16.
+//  Copyright © 2016 Apoorva Gupta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
